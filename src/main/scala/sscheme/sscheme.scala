2d79502694d637a1a1adfb4af2255cb7c607d5bb
Andre Silva <andre.beat@gmail.com>
2013-03-15 23:31:54 +0000

    implement toString

--- src/main/scala/sscheme/sscheme.scala
+++ src/main/scala/sscheme/sscheme.scala
@@ -4,16 +4,24 @@ import java.lang.Character
 import scala.util.parsing.combinator.{JavaTokenParsers, Parsers, RegexParsers}
 
 sealed trait LispVal extends Any
-case class Atom(val a: String) extends AnyVal with LispVal
+case class Atom(val a: String) extends AnyVal with LispVal {
+  override def toString = a
+}
 case class SList(val l: List[LispVal]) extends AnyVal with LispVal {
-  override def toString = s"SList([${l.mkString(", ")}])"
+  override def toString = l.mkString("(", " ", ")")
 }
-case class DottedList(val l: List[LispVal], val d: LispVal) extends LispVal {
-  override def toString = s"DottedList([${l.mkString(", ")}], $d)"
+case class DottedList(val h: List[LispVal], val t: LispVal) extends LispVal {
+  override def toString = s"(${h.mkString(" ")} . $t)"
 }
-case class Number(val n: Int) extends AnyVal with LispVal
-case class SString(val s: String) extends AnyVal with LispVal
-case class Bool(val b: Boolean) extends AnyVal with LispVal
+case class Number(val n: Int) extends AnyVal with LispVal {
+  override def toString = n.toString
+}
+case class SString(val s: String) extends AnyVal with LispVal {
+  override def toString = s""""$s""""
+}
+case class Bool(val b: Boolean) extends AnyVal with LispVal {
+  override def toString = if (b) "#t" else "#f"
+}
 
 trait SchemeTokenParsers extends JavaTokenParsers {
   def oneOf(chars: Char*)(kind: String): Parser[Elem] = {

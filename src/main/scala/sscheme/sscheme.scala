54db5000f8965a8814327b351da278038e3f0a34
Andre Silva <andre.beat@gmail.com>
2013-03-24 22:40:17 +0000

    conditionals

--- src/main/scala/sscheme/sscheme.scala
+++ src/main/scala/sscheme/sscheme.scala
@@ -117,13 +117,19 @@ object SchemePrimitives {
 }
 
 object SchemeInterpreter {
-  def apply(v: LispVal): LispVal = v match {
+  def eval(v: LispVal): LispVal = v match {
     case s: SString => s
     case n: Number => n
     case b: Bool => b
     case SList(List(Atom("quote"), v)) => v
-    case SList(Atom(func) :: args) => SchemePrimitives(func, args.map(apply))
+    case SList(List(Atom("if"), pred, conseq, alt)) => eval(pred) match {
+      case Bool(true) => eval(conseq)
+      case _ => eval(alt)
+    }
+    case SList(Atom(func) :: args) => SchemePrimitives(func, args.map(eval))
   }
+
+  def apply(v: LispVal): LispVal = eval(v)
 }
 
 trait SchemeTokenParsers extends JavaTokenParsers {
